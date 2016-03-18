3fb4491cd1484bfaac0c94a16813fe398b67f6ab
Jan Idziak <janidziak@gmail.com>
2016-03-18 10:14:51 +0100

    Update Topic modelling mallet wraper.R

--- R/Topic modelling mallet wraper.R
+++ R/Topic modelling mallet wraper.R
@@ -2,15 +2,25 @@ require(stylo)
 require(mallet)
 require(wordcloud)
 
-filtered_texts <- tag_and_filter_texts("corpus_test", "verb")
-prepared_data <- prepare_for_mallet(filtered_texts)
-model <- train(prepared_data)
-table_of_topics <- topic_table(model)
-table_of_topics$topics
-table_of_topics$words
-topic_wordcloud(table_of_topics)
+#' Function to train the mallet model. It is simple wrapper on the mallet library. 
+#' This solution is simplier and more intuitive for R users than initial functins. 
+#' The structure is simillar to one used in the caret and other packages for predictive modelling. 
+#' 
+#' @param text_array Array of texts used for the modelling.
+#' @param stoplist_file Txt file of the. List of the stopwords to be excluded from the text.
+#' @param token_regexp Regular expression to specify in the text.
+#' @param no_of_topics Number of topics to be modelled.
+#' @param ... Other parameters used for the modelling and the optimization of the topics.
+#' 
+#' @return topic.model The topic model from the mallet package
+#' @examples
+#' filtered_texts <- tag_and_filter_texts("corpus_test", "verb")
+#' prepared_data <- prepare_for_mallet(filtered_texts)
+#' model <- train(prepared_data)
+#' extract_article("http://www.gutenberg.org/files/51428/51428-0.txt", FALSE)
 
-train <- function(text_array, names = NULL, 
+
+train <- function(text_array, 
                   stoplist_file = "en.txt",
                   token_regexp = "[A-za-z]+",
                   no_of_topics = 20,
@@ -21,7 +31,7 @@ train <- function(text_array, names = NULL,
   
   if(is.null(names(text_array)))
     names <- 1:langth(text_array)
-  else 
+  else
     names <- names(text_array)
   mallet.instances = mallet.import(id.array = names, 
                                    text.array = text_array, 
@@ -45,6 +55,17 @@ train <- function(text_array, names = NULL,
 
 
 
+#' Function to calculate topics and words arrays from the mallet model. 
+#' 
+#' @param model Mallet model. 
+#' 
+#' @return topics Array of the topics.
+#' @return words Array of the most important words in topic.
+#' @examples
+#' table_of_topics <- topic_table(model)
+#' table_of_topics$topics
+#' table_of_topics$words
+
 topic_table <- function(model){
   doc.topics = mallet.doc.topics(topic.model, smoothed=T, normalized=T)
   topic.words = mallet.topic.words(topic.model, smoothed=T, normalized=T)
@@ -58,6 +79,7 @@ topic_table <- function(model){
   
 }
 
+
 topic_wordcloud<- function(topic_table, topic.id = 15, 
                            no.of.words = 100, rot.per = 0, 
                            random.order = FALSE){
@@ -66,3 +88,6 @@ topic_wordcloud<- function(topic_table, topic.id = 15,
                  random.order = random.order, 
                  rot.per = rot.per)
 }
+
+
+topic_wordcloud(table_of_topics)
